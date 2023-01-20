a357c10dd12cd8c217cc47ffbc17ca7bc7ab8203
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2023-01-20 08:41:33 +0530

    Update LICENSE, and other configs

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -2,7 +2,6 @@ module.exports = {
     parser: "@typescript-eslint/parser",
     extends: ["plugin:@typescript-eslint/recommended", "turbo", "prettier"],
     parserOptions: { "ecmaVersion": 2018, "sourceType": "module" },
-    extends: [],
     rules: {
 
     }
