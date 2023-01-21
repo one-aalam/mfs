2fd46412b79e10eada14853e32f8e74a9b492366
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2023-01-21 21:25:40 +0530

    Add TailwindCSS

--- apps/client/src/main.ts
+++ apps/client/src/main.ts
@@ -4,17 +4,19 @@ import { setupCounter } from './counter';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
+    <div class="flex flex-row">
+        <a href="https://vitejs.dev" target="_blank">
+            <img src="/vite.svg" class="logo" alt="Vite logo" />
+        </a>
+        <a href="https://www.typescriptlang.org/" target="_blank">
+            <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+        </a>
+    </div>
     <h1>Modern Full-stack</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
-    <p class="read-the-docs">
+    <p class="read-the-docs text-xl">
       Full-stack setup with Type-safety
     </p>
   </div>
